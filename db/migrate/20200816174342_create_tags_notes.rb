92c42c2d6eded5fc0e6433fa8304bebb99ac97a3
brandaoplaster <brandaoplaster@gmail.com>
2020-08-16 14:46:22 -0300

    Adding model tagsNotes

---
+++ db/migrate/20200816174342_create_tags_notes.rb
@@ -0,0 +1,10 @@
+class CreateTagsNotes < ActiveRecord::Migration[6.0]
+  def change
+    create_table :tags_notes do |t|
+      t.references :tag, null: false, foreign_key: true
+      t.references :note, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
