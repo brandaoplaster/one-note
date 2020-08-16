37b363113f9988ac00a4de8e41ee21adde92eb42
brandaoplaster <brandaoplaster@gmail.com>
2020-08-16 19:02:02 -0300

    Adding action create and private note_params method

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,5 +1,13 @@
 class NotesController < ApplicationController
+  
   def create
+    @note = Note.new(note_params.merge(user: current_user))
+
+    if @note.save
+      render json: { message: "Nota criada com sucesso!" }, status: :created
+    else
+      render json: { message: "Error ao criar a nota, tente mais tarde!" }, status: :unprocessable_entity
+    end
   end
 
   def update
@@ -13,4 +21,10 @@ class NotesController < ApplicationController
 
   def show
   end
+
+  private 
+
+    def note_params
+      params.require(:note).permit(:title, :body)
+    end
 end
