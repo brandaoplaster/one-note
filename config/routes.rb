33d24eb54a47a2e47734955358e387a9e7e2d549
brandaoplaster <brandaoplaster@gmail.com>
2020-08-17 14:41:53 -0300

    Created controller for tags

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :notes, only: [:create, :update, :index, :show, :destroy]
   post '/users/:id/add_note/:note_id', to: 'users#add_note'
   delete '/users/:id/remove_note/:note_id', to: 'users#remove_note'
+  post '/tags/:title/add_to_note/:note_id', to: 'tags#add_to_note'
+  delete '/tags/:title/remove_from_note/:note_id', to: 'tags#remove_from_note'
 end
