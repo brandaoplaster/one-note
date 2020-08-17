bde66d4b1aacfdfb6bd1e3c0f5a4c72e557f49bf
brandaoplaster <brandaoplaster@gmail.com>
2020-08-17 14:02:55 -0300

    Created controller for user to add and remove notes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :notes, only: [:create, :update, :index, :show, :destroy]
+  post '/users/:id/add_note/:note_id', to: 'users#add_note'
+  delete '/users/:id/remove_note/:note_id', to: 'users#remove_note'
 end
