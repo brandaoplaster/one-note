ad633b3e52fe08028005658e8459442c5958f099
brandaoplaster <brandaoplaster@gmail.com>
2020-08-22 21:52:05 -0300

    Created basic component show notes

---
+++ app/javascript/components/shownotes/index.vue
@@ -0,0 +1,65 @@
+<template>
+  <v-container class="grey lighten-5">
+    <!-- Columns start at 50% wide on mobile and bump up to 33.3% wide on desktop -->
+    <v-row>
+      <v-col v-for="n in 9" :key="n" cols="6" md="4">
+        <v-card class="pa-1" outlined tile>
+          <v-card-title>
+            <v-icon
+              large
+              left
+            >
+              mdi-twitter
+            </v-icon>
+            <span class="title font-weight-light">Twitter</span>
+          </v-card-title>
+
+          <v-card-text class="headline text-justify body-1">
+            "Turns out semicolon-less style is easier and safer in TS because most gotcha edge cases are type invalid as well."
+          </v-card-text>
+
+          <v-chip-group
+            v-model="selection"
+            active-class="deep-purple accent-4 white--text"
+            column
+          >
+            <v-chip>#ruby</v-chip>
+            <v-chip>#vuejs</v-chip>
+            <v-chip>#ruby on rails</v-chip>
+          </v-chip-group>
+
+          <v-card-actions>
+            <v-btn
+              text
+              color="deep-purple accent-4"
+            >
+              Read
+            </v-btn>
+            <v-btn
+              text
+              color="deep-purple accent-4"
+            >
+              Bookmark
+            </v-btn>
+            <v-spacer></v-spacer>
+            <v-btn icon>
+              <v-icon>mdi-heart</v-icon>
+            </v-btn>
+            <v-btn icon>
+              <v-icon>mdi-share-variant</v-icon>
+            </v-btn>
+          </v-card-actions>
+
+
+        </v-card>
+      </v-col>
+    </v-row>
+
+  </v-container>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -27,50 +27,8 @@
         
         </v-app-bar>
 
-        <v-layout row wrap pb-2>
-          <v-flex xs12 sm12 md12>
-
-            <v-card class="d-inline-block mx-auto">
-              <v-container>
-                <v-row justify="space-between">
-                  <v-col cols="auto">
-                    <v-img
-                      height="200"
-                      width="200"
-                      src="https://cdn.vuetifyjs.com/images/cards/store.jpg"
-                    ></v-img>
-                  </v-col>
-
-                  <v-col cols="auto" class="text-center pl-0">
-                    <v-row
-                      class="flex-column ma-0 fill-height"
-                      justify="center"
-                    >
-                      <v-col class="px-0">
-                        <v-btn icon>
-                          <v-icon>mdi-heart</v-icon>
-                        </v-btn>
-                      </v-col>
-
-                      <v-col class="px-0">
-                        <v-btn icon>
-                          <v-icon>mdi-bookmark</v-icon>
-                        </v-btn>
-                      </v-col>
-
-                      <v-col class="px-0">
-                        <v-btn icon>
-                          <v-icon>mdi-share-variant</v-icon>
-                        </v-btn>
-                      </v-col>
-                    </v-row>
-                  </v-col>
-                </v-row>
-              </v-container>
-            </v-card>
-
-          </v-flex>
-        </v-layout>
+        <ShowNotes></ShowNotes>
+        
       </v-container>
     </v-main>    
   </v-app>
@@ -88,6 +46,7 @@ export default {
 
   components: {
     Sidebar: () => import('../components/sidebar'),
+    ShowNotes: () => import('../components/shownotes'),
   },
 }
 </script>
