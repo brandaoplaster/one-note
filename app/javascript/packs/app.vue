5bb682d4bc0bcedcf523e91d442a0705c7ed2962
brandaoplaster <brandaoplaster@gmail.com>
2020-08-25 10:57:36 -0300

    Organizing components

--- app/javascript/packs/app.vue
+++ app/javascript/packs/app.vue
@@ -1,51 +1,19 @@
 <template>
   <v-app>
-    <Sidebar></Sidebar>
-
-    <v-main id="home">
-      <v-container fluid ma-0 pa-1>
-        <v-app-bar color="blue-grey darken-3" dark>
-          <v-btn class="ma-2" color="orange darken-2" dark>
-            <v-icon left>mdi-pencil</v-icon> Edita
-          </v-btn>
-
-          <v-btn class="ma-2" color="orange darken-2" dark>
-            <v-icon left>mdi-content-save</v-icon> Salvar
-          </v-btn>
-
-          <v-btn class="ma-2" color="orange darken-2" dark>
-            <v-icon dark left>mdi-arrow-left</v-icon>Back
-          </v-btn>
-
-          <v-btn class="ma-2" color="orange darken-2" dark>
-            <v-icon left>mdi-delete</v-icon> Delete
-          </v-btn>
-
-          <v-btn class="ma-2" color="orange darken-2" dark>
-            <v-icon left>mdi-share-variant</v-icon> Compartilha
-          </v-btn>          
-        
-        </v-app-bar>
-
-        <ShowNotes></ShowNotes>
-      </v-container>
-    </v-main>    
+    <sidebar></sidebar>
+    <home></home>
   </v-app>
 </template>
 
 <script>
 export default {
   name: 'App',
-  props: {
-    source: String,
-  },
-    data: () => ({
-      
-    }),
+  
+  data: () => ({}),
 
   components: {
     Sidebar: () => import('../components/sidebar'),
-    ShowNotes: () => import('../components/shownotes'),
+    Home: () => import('../components/home');
   },
 }
 </script>
