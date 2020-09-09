cb0603837a9382e881b89280d7f55e4ff5fb8502
brandaoplaster <brandaoplaster@gmail.com>
2020-09-08 23:18:26 -0300

    Modifying controller tag adding create destroy methods

--- app/controllers/api/v1/tags_controller.rb
+++ app/controllers/api/v1/tags_controller.rb
@@ -1,17 +1,20 @@
 class Api::V1::TagsController < ApplicationController
+  before_action :set_tags, only: [:update, :destroy]
 
   def index
     @tags = current_user.tags
     render :json => @tags.to_json()
   end
 
-  def add_to_note
-    @tag = Tag.find_or_create_by(title: params[:title], user: current_user)
-    @note = Note.find(params[:note_id])
-    @note.tags << @tag
+  def create
+    @tag = Tag.find_or_create_by(tag_params)
+    render json: { message: "Tag criada com sucesso!" }, status: :ok
   end
 
-  def remove_from_note
+  def update
+  end
+
+  def destroy
     @tag = Tag.find_by(title: params[:title], user: current_user)
     @note = Note.find(params[:note_id])
     @note.tags.delete(@tag.id)
@@ -19,4 +22,14 @@ class Api::V1::TagsController < ApplicationController
 
     render json: { message: "Tag removida com sucesso!" }, status: :ok
   end
+
+  private
+
+    def tag_params
+      params.require(:tag).permit(:title).merge(user: current_user)
+    end
+
+    def set_tag
+      @note = Note.find(params[:note_id])
+    end
 end
