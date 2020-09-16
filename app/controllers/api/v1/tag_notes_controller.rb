931252c69ab188e45769e2fab7a068a8a9d83d53
brandaoplaster <brandaoplaster@gmail.com>
2020-09-15 23:10:50 -0300

    Adding action create to save link tag with note

--- app/controllers/api/v1/tag_notes_controller.rb
+++ app/controllers/api/v1/tag_notes_controller.rb
@@ -1,7 +1,19 @@
 class Api::V1::TagNotesController < ApplicationController
   def create
+    @tag_note = TagsNote.new(tag_notes_params)
+    if @tag_note.save
+      render json: { message: "Adicionado tag a esta note com sucesso!" }, status: :created
+    else
+      render :json { message: "Erro ao adicionar tag a esta note!" }, status: :unprocessable_entity
+    end
   end
 
   def destroy
   end
+
+  private
+
+    def tag_notes_params
+      params.require(:tag_note).permit(:tag_id, :note_id)
+    end
 end
