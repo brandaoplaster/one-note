ac62d2913893f5b2a3cbf2acdf83c598f04da944
brandaoplaster <brandaoplaster@gmail.com>
2020-09-30 22:00:30 -0300

    Does not allow favorite note twice and return error message

--- app/controllers/api/v1/favorites_controller.rb
+++ app/controllers/api/v1/favorites_controller.rb
@@ -8,13 +8,13 @@ class Api::V1::FavoritesController < ApplicationController
 
   def create
     count_favorite = Favorite.where(user_id: current_user.id).count
-    if count_favorite < 2
+    if count_favorite < 5
       @favoritable = Favorite.new(favorite_params)
 
       if @favoritable.save
         render json: { message: 'Successfully favored!' }, status: :created
       else
-        render json: { message: 'Error favorite this note!' }, status: :unprocessable_entity
+        render json: { message: @favoritable.errors.full_messages }, status: :unprocessable_entity
       end
     else
       render json: { message: "You can't favorite more than 5 notes" }, status: :unprocessable_entity
